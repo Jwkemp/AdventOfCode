68dbb564b867e9aacdf924541a994c711092ab10
Jack Kemp <JackWGKemp@gmail.com>
2018-07-13 15:30:52 +0100

    Day 21 Complete! Finally!

--- AdventOfCode/Day21.cs
+++ AdventOfCode/Day21.cs
@@ -1,110 +1,201 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AdventOfCode.Tools;
+
 
 namespace AdventOfCode
 {
-	class Day21
-	{
-		public static int Part1()
-		{
-            List<ArtistRule> artistRules = new List<ArtistRule>();
-
-            var lines = Properties.Resources.input_D21.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            foreach (string line in lines)
-            {
-                string input = line.Substring(0, line.IndexOf(' '));
-                Matrix<bool> m;
-                if (input.Length == 5)
-                {
-                    m = new Matrix<bool>(
-                        input[0] == '.', input[1] == '.', 
-                        input[3] == '.', input[4] == '.');
-                }
-                else
-                {
-                    m = new Matrix<bool>(
-                        input[0] == '.', input[1] == '.', input[2]  == '.',
-                        input[4] == '.', input[5] == '.', input[6]  == '.',
-                        input[8] == '.', input[9] == '.', input[10] == '.');
-                }
-
-                string output = line.Substring(line.IndexOf('>')+2);
-                Matrix<bool> n;
-                if (output.Length == 5)
-                {
-                    n = new Matrix<bool>(
-                        output[0] == '.', output[1] == '.',
-                        output[3] == '.', output[4] == '.');
-                }
-                else if (output.Length == 11 )
-                {
-                    n = new Matrix<bool>(
-                        output[0] == '.', output[1] == '.', output[2]  == '.',
-                        output[4] == '.', output[5] == '.', output[6]  == '.',
-                        output[8] == '.', output[9] == '.', output[10] == '.');
-                }
-                else
-                {
-                    n = new Matrix<bool>(
-                        output[0]  == '.', output[1]  == '.', output[2]  == '.', output[3]  == '.',
-                        output[5]  == '.', output[6]  == '.', output[7]  == '.', output[8]  == '.',
-                        output[10] == '.', output[11] == '.', output[12] == '.', output[13] == '.',
-                        output[15] == '.', output[16] == '.', output[17] == '.', output[18] == '.');                   
-                }
-                artistRules.Add(new ArtistRule(m, n));
-            }     
-
-            // if grid length % 2
-            // rules of 2 
-
-            // else 
-            // rules of 3
-
-
-
-
-
-            return 0;
-		}
-
-		public static int Part2()
-		{
-
-			return 0;
-		}		
-
-	}
-
-    class ArtistRule
+    public static class Extensions
     {
-        public List<Matrix<bool>> rules;
-        public Matrix<bool> output;
-
-        public ArtistRule(Matrix<bool> inputRule, Matrix<bool> outputRule )
+        public static void TryAdd<k, v>(this Dictionary<k, v> dictionary, k key, v value)
         {
-            rules = new List<Matrix<bool>>();
-
-            Matrix<bool> b = Matrix<bool>.Rotate90(inputRule);
-            Matrix<bool> c = Matrix<bool>.Rotate90(b);
-            Matrix<bool> d = Matrix<bool>.Rotate90(c);
-
-            rules.Add(inputRule);
-            rules.Add(b);
-            rules.Add(c);
-            rules.Add(d);
-            rules.Add(Matrix<bool>.Flip(inputRule));
-            rules.Add(Matrix<bool>.Flip(b));
-            rules.Add(Matrix<bool>.Flip(c));
-            rules.Add(Matrix<bool>.Flip(d));
-
-            output = outputRule;
+            if (!dictionary.ContainsKey(key))
+            {
+                dictionary.Add(key, value);
+            }
         }
-
     }
 
+    class Day21
+    {
 
+        public static int Part1()
+        {
+            return Solve(5);
+        }
+
+        public static int Part2()
+        {
+            return Solve(18);
+        }
+
+        private static int Solve(int interations )
+        {
+            string[] rules = Properties.Resources.input_D21.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            var separators = new char[] { ' ', '=', '>' };
+
+            Dictionary<string, string> rulesMap = new Dictionary<string, string>();
+
+            foreach (var rule in rules)
+            {
+                var tokens = rule.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                string from = tokens[0];
+                string to = tokens[1];
+
+                for (int i = 0; i < 4; i++)
+                {
+                    var newFrom = Rotate(from);
+                    rulesMap.TryAdd(newFrom, to);
+                    rulesMap.TryAdd(FlipHorizontal(newFrom), to);
+                    rulesMap.TryAdd(FlipVertical(newFrom), to);
+
+                    from = newFrom;
+                }
+            }
+
+            string[] grid = new string[]
+            {
+            ".#.",
+            "..#",
+            "###",
+            };
+
+            grid = Enhance(iterations: 18, grid: grid, rules: rulesMap);
+
+            return CountActivePixels(grid);
+        }
+
+        private static string FlipHorizontal(string grid)
+        {
+            string[] rows = grid.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] newRows = new string[rows.Length];
+
+            for (int i = 0; i < rows.Length; i++)
+            {
+                newRows[i] = string.Join("", rows[i].Reverse());
+            }
+
+            return string.Join<string>("/", newRows);
+        }
+
+        private static string FlipVertical(string grid)
+        {
+            string[] rows = grid.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] newRows = new string[rows.Length];
+
+            for (int i = 0; i < rows.Length; i++)
+            {
+                newRows[rows.Length - i - 1] = rows[i];
+            }
+
+            return string.Join<string>("/", newRows);
+        }
+
+        private static string Rotate(string grid)
+        {
+            string[] rows = grid.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            char[,] newRows = new char[rows.Length, rows.Length];
+
+            for (int i = 0; i < rows.Length; i++)
+            {
+                for (int j = 0; j < rows.Length; j++)
+                {
+                    newRows[rows.Length - j - 1, i] = rows[i][j];
+                }
+            }
+
+            string[] sNewRows = new string[rows.Length];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                for (int j = 0; j < rows.Length; j++)
+                {
+                    sNewRows[i] += newRows[i, j];
+                }
+            }
+
+            string result = string.Join("/", sNewRows);
+            return result;
+        }
+
+        private static string CopyFrom(string[] grid, int startRow, int startColumn, int num)
+        {
+            string[] section = new string[num];
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    section[i] += grid[i + startRow][j + startColumn];
+                }
+            }
+
+            return string.Join("/", section);
+        }
+
+        private static void CopyTo(string[] grid, string section, int size, int startRow, int startColumn)
+        {
+            string[] rows = section.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    grid[startRow + i] += rows[i][j];
+                }
+            }
+        }
+
+        private static string[] EnhanceStep(string[] grid, Dictionary<string, string> rules, int size)
+        {
+            int newSize = size + 1;
+
+            string[] newGrid = new string[grid.Length / size * newSize];
+
+            for (int j = 0; j * size < grid.Length; j++)
+            {
+                for (int k = 0; k * size < grid.Length; k++)
+                {
+                    string section = CopyFrom(grid, j * size, k * size, size);
+                    CopyTo(newGrid, rules[section], newSize, j * newSize, k * newSize);
+                }
+            }
+
+            return newGrid;
+        }
+
+        private static string[] Enhance(int iterations, string[] grid, Dictionary<string, string> rules)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                if (grid.Length % 2 == 0)
+                {
+                    grid = EnhanceStep(grid, rules, size: 2);
+                }
+                else // % 3 == 0
+                {
+                    grid = EnhanceStep(grid, rules, size: 3);
+                }
+            }
+
+            return grid;
+        }
+
+        private static int CountActivePixels(string[] grid)
+        {
+            int countOn = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid.Length; j++)
+                {
+                    if (grid[i][j] == '#')
+                    {
+                        countOn++;
+                    }
+                }
+            }
+
+            return countOn;
+        }
+    }
 }
